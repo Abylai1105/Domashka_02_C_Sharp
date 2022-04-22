6bdfe375c2b084f2c3623c8770c3c8cc3cdb6cc4
Abylai <aba.omar@mail.ru>
2022-04-22 13:30:28 +0600

    add Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,20 @@
+﻿int number;
+Console.WriteLine("Введите трехзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+if(number > 99)
+{
+    if(number < 1000)
+    {
+        number = number/10;
+        int result = number%10;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Данные не соответствуют");
+    }
+}
+else
+{
+    Console.WriteLine("Данные не соответствуют");
+}
