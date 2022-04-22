95fd88410b841e42807c31756a27dc9e011422dd
Abylai <aba.omar@mail.ru>
2022-04-22 13:51:36 +0600

    add Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,8 @@
+﻿//using System;
+//using static System.Console;
+//Console.Clear();
+
+int n;
+Console.WriteLine("Введите число: ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n > 99 ? n.ToString()[2] :n < -99 ? n.ToString()[3]: "Третьего символа нет");
