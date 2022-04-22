c70a9bc6ae5c8524fa20748e166cf194d38ac15c
Abylai <aba.omar@mail.ru>
2022-04-22 14:00:05 +0600

    add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,18 @@
+﻿int n;
+Console.WriteLine("Введите число: ");
+n = Convert.ToInt32(Console.ReadLine());
+if (n > 0)
+{
+    if(n < 8)
+    {
+        Console.WriteLine(n == 6 ? "Weekend": n == 7 ? "Weekend" : "Workday");
+    }
+    else
+    {
+        Console.WriteLine("Не найден день недели");
+    }
+}
+else
+{
+    Console.WriteLine("Не найден день недели");
+}
